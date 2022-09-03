9b017dc11cb45deed9181a3671da343e44c391a7
Vlad <vladi003@yandex.ru>
2022-09-03 06:57:10 +0300

    bug fixes

--- kirumi/mainapp/templatetags/product_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-
-register = template.Library()
-
-@register.filter
-def is_active(values):
-    values = [val for val in values if val.is_active==True]
-    return values
